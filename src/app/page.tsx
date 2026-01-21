985ad9e5c1f96efef6f372616a52df2b04cac1ee
jjbro <irusmage@gmail.com>
2026-01-21 23:55:01 +0900

    Skip server report fetch for faster initial load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
 import HomeClient from "@/components/HomeClient";
-import { getFullReport } from "@/lib/report-server";
 import { getKrxSymbols } from "@/lib/krx";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  // Fetch initial data on the server to improve LCP
-  const [initialReportResult, initialSymbols] = await Promise.all([
-    getFullReport("005930.KS"),
-    getKrxSymbols(),
-  ]);
+  // Keep initial load lightweight; client will fetch report data.
+  const initialSymbols = await getKrxSymbols();
 
   return (
-    <HomeClient 
-      initialReport={initialReportResult} 
-      initialSymbols={initialSymbols} 
-    />
+    <HomeClient initialSymbols={initialSymbols} />
   );
 }
