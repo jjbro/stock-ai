9779fe9b990c8b3a908ef27476a6bedc78059f85
jjbro <irusmage@gmail.com>
2026-01-21 23:39:52 +0900

    Force dynamic page to avoid build-time fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import HomeClient from "@/components/HomeClient";
 import { getFullReport } from "@/lib/report-server";
 import { getKrxSymbols } from "@/lib/krx";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   // Fetch initial data on the server to improve LCP
   const [initialReportResult, initialSymbols] = await Promise.all([
